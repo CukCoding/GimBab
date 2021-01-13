f82f4550f7fa894d4f0ca884575023514aea7596
1876070677 <fixerpremium@gmail.com>
2021-01-13 16:04:22 +0900

    회의실문제 3번째 시도 실패

--- 2주차/1931.py
+++ 2주차/1931.py
@@ -78,3 +78,68 @@ while True :
 
 print(meeting)
 '''
+def setPivot(start, mid, end, index) :
+    if arr[mid][index] < arr[start][index] :
+        swap(mid, start)
+    if arr[mid][index] > arr[end][index] :
+        swap(mid, end)
+    if arr[mid][index] < arr[start][index] :
+        swap(mid, start)
+
+
+def swap(a, b) :
+    temp1 = arr[b][0]
+    temp2 = arr[b][1]
+
+    arr[b][0] = arr[a][0]
+    arr[b][1] = arr[a][1]
+
+    arr[a][0] = temp1
+    arr[a][1] = temp2
+
+def quickSort(start, end, index) :
+    mid = ((end - start) // 2) + start
+
+    setPivot(start, mid, end, index)
+
+    if end - start < 3 :
+        return
+
+    swap(mid, end - 1)
+    pivot = end - 1
+    i = start + 1
+    j = end - 1
+
+    while i < j :
+        while i < end and arr[i][index] <= arr[pivot][index] :
+            i += 1
+
+        while j > start and arr[j][index] >= arr[pivot][index] :
+            j -= 1
+
+        if i < j :
+            swap(i, j)
+
+    if i > pivot and j < pivot :
+        quickSort(start, i - 1, index)
+        return
+
+    swap(i, pivot)
+    quickSort(start, i - 1, index)
+    quickSort(i + 1, end, index)
+
+
+cnt = int(input())
+arr = [list(map(int, input().split())) for _ in range(cnt)]
+endTime = 0
+meeting = 0
+
+for index in range(0, 2) :
+    quickSort(0, cnt-1, index)
+
+for i in range(0, cnt) :
+    if arr[i][0] >= endTime :
+        meeting += 1
+        endTime = arr[i][1]
+print(meeting)
+
