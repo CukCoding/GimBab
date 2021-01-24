c3c2031f9a70ace2747afc82e566acaf2dfcf9bd
1876070677 <fixerpremium@gmail.com>
2021-01-24 21:52:22 +0900

    2580 1번코드 주석 삭제

--- 3주차/2580-1.py
+++ 3주차/2580-1.py
@@ -97,42 +97,6 @@ def inputNumber(location, number) :
         sq[temp][0] -= 1
         return False
 
-'''
-def inputNumber(location, number) :
-    x = blank[location][0]
-    y = blank[location][1]
-    quadX = (x // 3) * 3
-    quadY = (y // 3) * 3
-    cnt = 0
-
-
-    for j in range(9):
-        if sudoku[x][j] == number:
-            cnt += 1
-            break
-        elif sudoku[j][y] == number:
-            cnt += 1
-            break
-        elif sudoku[quadX + (j // 3)][quadY + (j % 3)] == number:
-            cnt += 1
-            break
-
-    if cnt > 0 :
-        return False
-    elif location + 1 == blankCnt :
-        sudoku[x][y] = number
-        return True
-    else :
-        sudoku[x][y] = number
-        for i in range(1, prob[location + 1][0] + 1) :
-            if inputNumber(location + 1, prob[location + 1][i]) == False :
-                continue
-            else :
-                return True
-
-        sudoku[x][y] = 0
-        return False
-'''
 
 wid = [[0 for _ in range(10)] for _ in range(10)]
 hei = [[0 for _ in range(10)] for _ in range(10)]
