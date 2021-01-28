9bcb18b4ff8f4fe22de3ef1b86a47274b064c4c8
1876070677 <fixerpremium@gmail.com>
2021-01-28 19:04:37 +0900

    파일 이름 수정

--- 4주차/7562-1.py
+++ 4주차/7562-1.py
@@ -1,9 +1,20 @@
 '''
-    두번째 코드 : 따로 Queue를 사용하지 않고 리스트에 append와 del을 이용해서 다음에 탐색할 좌표들을 저장한다.
-    시간 단축 : 내가 원하는 좌표에 도달시 clear를 이용해서 다음 탐색해야할 좌표를 완전히 비우면
-               while문 조건에 의해서 탈출되며 Solve()함수가 종료된다.
+    원리 : BFS // 시간초과
+    파이썬 Queue()는 put, get 두가지의 메소드로 작동
+    첫번째 좌표를 queue에 넣는 것으로 시작
+    큐에서 좌표를 꺼내서 나이트가 이동할 수 있는 범위들을 하나하나 체크(체스판 안의 좌표인지)
+    그러고 나서 이동할 수 있는 곳이라면 몇번째로 이동가능한 좌표인지 숫자를 마킹하고 큐에 넣는다
+
+    탐색하다가 똑같은 좌표에 여러번 도달할 수 있는데,
+    예를들어 2번째로 도착할 수 있는 좌표가 5번째 이동으로도 도착할수 있다면, 그럴때는 5라는 숫자를 저장하면 안된다
+    왜냐하면 최솟값을 찾아야하므로..
+    그래서 조건문에서 해당 칸이 0일 경우(즉, 방문을 한번도 하지않은 좌표)에만 값을 넣어주는형식이다.
+
+    마지막 결과문에서 내가 원하는 도착지점의 좌표에 들어가있는 숫자를 프린트 하면
+    최솟값을 얻을 수 있다.
 '''
 from sys import stdin
+from queue import Queue
 
 def check(x, y) :
     if x >= 0 and x < pos[0] and y >= 0 and y < pos[0] :
@@ -12,26 +23,22 @@ def check(x, y) :
         return 0
 
 def solve() :
-    x = pos[1][0]
-    y = pos[1][1]
-    chess[x][y] = 1
-    visit.append([x, y])
-    while len(visit) > 0 :
-        x = visit[0][0]
-        y = visit[0][1]
-        del visit[0]
+    chess[pos[1][0]][pos[1][1]] = 1
+    visit.put([pos[1][0], pos[1][1]])
+
+    while visit.qsize() > 0 :
+        x, y = visit.get()
         for i in range(8) :
             tx = x + moveX[i]
             ty = y + moveY[i]
             if check(tx, ty) == 1 and chess[tx][ty] == 0 :
                 chess[tx][ty] = chess[x][y] + 1
-                visit.append([tx, ty])
-                if tx == pos[2][0] and ty == pos[2][1] :
-                    visit.clear()
-                    break
+                visit.put([tx,ty])
+
+
 moveX = [-2, -1, 1, 2, -2, -1, 1, 2]
 moveY = [-1, -2, -2, -1, 1, 2, 2, 1]
-visit = []
+visit = Queue()
 result = []
 testcase = int(stdin.readline())
 
