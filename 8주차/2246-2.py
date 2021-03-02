178d6a7c41058d587134f5b53f5e7ee78937b93b
1876070677 <fixerpremium@gmail.com>
2021-03-02 20:30:42 +0900

    2246 second try, timeout..

---
+++ 8주차/2246-2.py
@@ -0,0 +1,30 @@
+'''
+    일일히 다 비교하는것이라서 50% 이상에서 시간초과가 뜬다
+'''
+import sys
+r = sys.stdin.readline
+
+N = int(r())
+P = []
+cnt = 0
+
+for i in range(N) :
+    P.append(list(map(int, r().split())))
+
+for i in range(N) :
+    d = P[i][0]
+    c = P[i][1]
+    pos = 0
+    for j in range(N) :
+        if i == j :
+            continue
+        elif P[j][1] < c and P[j][0] <= d : #가격은 싼데 거리가 현재 거리보다 가깝거나 또는 같으면 브레이크 조건
+            pos = 1
+            break
+        elif P[j][0] < d and P[j][1] <= c : #거리는 가까운데 가격이 지금거 보다 싸면 브레이크 조건
+            pos = 1
+            break
+    if pos == 0 :
+        cnt += 1
+
+print(cnt)
