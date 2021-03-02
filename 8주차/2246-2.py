eec525ce0ca834e5b96aaf36813f6181dc73e66d
1876070677 <fixerpremium@gmail.com>
2021-03-02 21:45:38 +0900

    2246 use sort library but Timeout..

--- 8주차/2246-2.py
+++ 8주차/2246-2.py
@@ -16,14 +16,13 @@ for i in range(N) :
     c = P[i][1]
     pos = 0
     for j in range(N) :
-        if i == j :
-            continue
-        elif P[j][1] < c and P[j][0] <= d : #가격은 싼데 거리가 현재 거리보다 가깝거나 또는 같으면 브레이크 조건
-            pos = 1
-            break
-        elif P[j][0] < d and P[j][1] <= c : #거리는 가까운데 가격이 지금거 보다 싸면 브레이크 조건
-            pos = 1
-            break
+        if i != j :
+            if P[j][1] < c and P[j][0] <= d:  # 가격은 싼데 거리가 현재 거리보다 가깝거나 또는 같으면 브레이크 조건
+                pos = 1
+                break
+            if P[j][0] < d and P[j][1] <= c:  # 거리는 가까운데 가격이 지금거 보다 싸면 브레이크 조건
+                pos = 1
+                break
     if pos == 0 :
         cnt += 1
 
